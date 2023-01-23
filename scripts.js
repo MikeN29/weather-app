0f8ae643ec71ab38109224f49c0f4a075f997c5e
Michael Norman <mikenorman192@msn.com>
2023-01-23 13:53:28 +0000

    inital setup

---
+++ scripts.js
@@ -0,0 +1,9 @@
+//http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+//e9abf275dd62a4be25c1a660d12a04fd 
+
+
+`http://api.openweathermap.org/geo/1.0/direct?q=${USERINPUTVARIABLE}&appid=e9abf275dd62a4be25c1a660d12a04fd`
+
+// https://api.openweathermap.org/data/2.5/weather?lat=51.45&lon=-2.59&appid=e9abf275dd62a4be25c1a660d12a04fd&units=metric
+
