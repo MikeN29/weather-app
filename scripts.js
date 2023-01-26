cc4b2c47e61b16ef45906fac991fdc5c5561791d
Michael Norman <mikenorman192@msn.com>
2023-01-26 14:04:36 +0000

    tidy

--- scripts.js
+++ scripts.js
@@ -7,6 +7,8 @@
 
 // https://api.openweathermap.org/data/2.5/weather?lat=51.45&lon=-2.59&appid=e9abf275dd62a4be25c1a660d12a04fd&units=metric
 
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=e9abf275dd62a4be25c1a660d12a04fd&units=metric
+
 
 
 let userLocation = '';
@@ -39,20 +41,11 @@ function locationAPI(userLocation) {
         fetch(weatherAPI)
         .then(response => response.json()  )
         .then(content => {
-          console.log("current temp: " + content.main.temp);
+
             let currentTemp = content.main.temp;
-            //console.log(currentTemp);
-
-          console.log("current weather condition: " + content.weather[0].main);
             let currentWeatherCondition = content.weather[0].main;
-
-          console.log("current temp high: " + content.main.temp_max);
             let currentTempHigh = content.main.temp_max;
-
-          console.log("current temp low: " + content.main.temp_min);
             let currentTempLow = content.main.temp_min;
-
-          console.log(content.main)
 
           createWeatherCard(userLocation, currentTemp, currentWeatherCondition, currentTempHigh, currentTempLow)
     
