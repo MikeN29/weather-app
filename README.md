c027063861000cdc5db432acf946b0932c41bc73
Michael Norman <mikenorman192@msn.com>
2023-02-15 19:26:00 +0000

    updated readme

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 Live Preview: https://miken29.github.io/weather-app/
 
-Happy with the outcome however my code is WET!
+Outcomes:
 
-I think as a learning point to develop this project I would like to refactor my code and 
-get rid of where I am repeating myself. Some good things to work on!
-
+- This is a Weather App which takes a users input location and displays current and future forecast.
+- Pulls data from an API
+- I am appy with the outcome however I think my code is WET!
+- I think as a learning point to develop this project I would like to refactor my code and get rid of where I am repeating myself. Some good things to work on!
--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 //http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
-//e9abf275dd62a4be25c1a660d12a04fd 
-
+//e9abf275dd62a4be25c1a660d12a04fd
 
 // `http://api.openweathermap.org/geo/1.0/direct?q=${userLocation}&appid=e9abf275dd62a4be25c1a660d12a04fd`
 
@@ -9,127 +8,131 @@
 
 // api.openweathermap.org/data/2.5/forecast?lat=51.45&lon=2.59&appid=e9abf275dd62a4be25c1a660d12a04fd&units=metric
 
-
-
-let userLocation = '';
-let latVar = ''; 
-let lonVar = ''; 
-
+let userLocation = "";
+let latVar = "";
+let lonVar = "";
 
 function submitData() {
-    let userLocation = document.getElementById('location-search').value // make capital first letters!
-    console.log(userLocation)
+  let userLocation = document.getElementById("location-search").value; // make capital first letters!
+  console.log(userLocation);
 
-    locationAPI(userLocation)
-    document.getElementById("submit").disabled = true;
-
+  locationAPI(userLocation);
+  document.getElementById("submit").disabled = true;
 }
 
 function resetData() {
+  document.getElementById("submit").disabled = false;
 
-    document.getElementById("submit").disabled = false;
+  //remove classes
+  let element = document.getElementById("weatherCardContainer");
+  element.classList.remove("weatherCardContainer");
 
-    //remove classes
-    let element = document.getElementById("weatherCardContainer");
-    element.classList.remove("weatherCardContainer");
+  const elementsList = document.querySelectorAll(
+    "#tomorrowCard, #dayThreeCard, #dayFourCard"
+  );
+  const elementsArray = [...elementsList];
 
-    const elementsList = document.querySelectorAll("#tomorrowCard, #dayThreeCard, #dayFourCard");
-    const elementsArray = [...elementsList];
-  
-    elementsArray.forEach(element => {
-      element.classList.remove("dayThreeCards");
-    });
+  elementsArray.forEach((element) => {
+    element.classList.remove("dayThreeCards");
+  });
 
-    //remove children
+  //remove children
 
-    const myNode = document.getElementById("weatherCardContainer");
-    while (myNode.firstChild) {
-      myNode.removeChild(myNode.lastChild);
+  const myNode = document.getElementById("weatherCardContainer");
+  while (myNode.firstChild) {
+    myNode.removeChild(myNode.lastChild);
+  }
+
+  elementsArray.forEach((element) => {
+    while (element.firstChild) {
+      element.removeChild(element.lastChild);
     }
-
-    elementsArray.forEach(element => {
-        while (element.firstChild) {
-            element.removeChild(element.lastChild);
-          }
-      });
+  });
 }
 
 function locationAPI(userLocation) {
-    let locationAPI = `https://api.openweathermap.org/geo/1.0/direct?q=${userLocation}&appid=e9abf275dd62a4be25c1a660d12a04fd`
-    //console.log(locationAPI)
-    event.preventDefault()
+  let locationAPI = `https://api.openweathermap.org/geo/1.0/direct?q=${userLocation}&appid=e9abf275dd62a4be25c1a660d12a04fd`;
+  //console.log(locationAPI)
+  event.preventDefault();
 
-    fetch(locationAPI)
-      .then(response => response.json()  )
-      .then(content => {
-        let latVar = content[0].lat;
-        let lonVar = content[0].lon;
-        weatherAPI(userLocation, latVar, lonVar)
-
+  fetch(locationAPI)
+    .then((response) => response.json())
+    .then((content) => {
+      let latVar = content[0].lat;
+      let lonVar = content[0].lon;
+      weatherAPI(userLocation, latVar, lonVar);
     })
-    .catch(err =>{
-        console.error(err)
-    })
-
+    .catch((err) => {
+      console.error(err);
+    });
 }
 
 function weatherAPI(userLocation, latVar, lonVar) {
+  let weatherAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${latVar}&lon=${lonVar}&appid=e9abf275dd62a4be25c1a660d12a04fd&units=metric`;
 
-        let weatherAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${latVar}&lon=${lonVar}&appid=e9abf275dd62a4be25c1a660d12a04fd&units=metric`
+  fetch(weatherAPI)
+    .then((response) => response.json())
+    .then((content) => {
+      let currentTemp = content.main.temp;
+      let currentWeatherCondition = content.weather[0].main;
+      let currentTempHigh = content.main.temp_max;
+      let currentTempLow = content.main.temp_min;
 
-        fetch(weatherAPI)
-        .then(response => response.json()  )
-        .then(content => {
-
-            let currentTemp = content.main.temp;
-            let currentWeatherCondition = content.weather[0].main;
-            let currentTempHigh = content.main.temp_max;
-            let currentTempLow = content.main.temp_min;
-
-          createWeatherCard(userLocation, currentTemp, currentWeatherCondition, currentTempHigh, currentTempLow)
-          threeDayForecast(userLocation, latVar, lonVar)
-    
-      })
-      .catch(err =>{
-          console.error(err)
-      })
-
+      createWeatherCard(
+        userLocation,
+        currentTemp,
+        currentWeatherCondition,
+        currentTempHigh,
+        currentTempLow
+      );
+      threeDayForecast(userLocation, latVar, lonVar);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
 
+function createWeatherCard(
+  userLocation,
+  currentTemp,
+  currentWeatherCondition,
+  currentTempHigh,
+  currentTempLow
+) {
+  const weatherIMG = document.createElement("img");
+  const ul = document.createElement("ul");
 
-function createWeatherCard(userLocation, currentTemp, currentWeatherCondition, currentTempHigh, currentTempLow) {
-    const weatherIMG = document.createElement('img');
-    const ul = document.createElement('ul');
+  document.getElementById("weatherCardContainer").appendChild(weatherIMG);
+  document.getElementById("weatherCardContainer").appendChild(ul);
 
-    document.getElementById("weatherCardContainer").appendChild(weatherIMG);
-    document.getElementById("weatherCardContainer").appendChild(ul);
+  weatherIMG.src = `./images/${currentWeatherCondition}.png`;
 
-    weatherIMG.src = `./images/${currentWeatherCondition}.png`;
+  let element = document.getElementById("weatherCardContainer");
+  element.classList.add("weatherCardContainer");
 
-    let element = document.getElementById("weatherCardContainer");
-    element.classList.add("weatherCardContainer");
+  array = [
+    `${userLocation}`,
+    `${currentWeatherCondition}`,
+    `Current Temp: ${currentTemp}&#176;C`,
+    `Daily high: ${currentTempHigh}&#176;C`,
+    `Daily Low: ${currentTempLow}&#176;C`,
+  ];
 
-
-    array = [`${userLocation}`, `${currentWeatherCondition}`, `Current Temp: ${currentTemp}&#176;C`, `Daily high: ${currentTempHigh}&#176;C`, `Daily Low: ${currentTempLow}&#176;C`]
-
-    for (var i=0; i<array.length; i++){
-        var li=document.createElement('li');
-        ul.appendChild(li);
-        li.innerHTML=li.innerHTML + array[i];
-    }
+  for (var i = 0; i < array.length; i++) {
+    var li = document.createElement("li");
+    ul.appendChild(li);
+    li.innerHTML = li.innerHTML + array[i];
+  }
 }
 
 function threeDayForecast(userLocation, latVar, lonVar) {
-
   let threeDayForecastAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${latVar}&lon=${lonVar}&appid=e9abf275dd62a4be25c1a660d12a04fd&units=metric`;
 
-
-  console.log(threeDayForecastAPI)
+  console.log(threeDayForecastAPI);
 
   fetch(threeDayForecastAPI)
-  .then(response => response.json()  )
-  .then(content => {
-
+    .then((response) => response.json())
+    .then((content) => {
       let tomorrowTemp = content.list[7].main.temp;
       let tomorrowWeatherCondition = content.list[7].weather[0].main;
       let tomorrowTempHigh = content.list[7].main.temp_max;
@@ -145,112 +148,137 @@ function threeDayForecast(userLocation, latVar, lonVar) {
       let dayFourTempHigh = content.list[23].main.temp_max;
       let dayFourTempLow = content.list[23].main.temp_min;
 
-      threeDayCard(tomorrowTemp, tomorrowWeatherCondition, tomorrowTempHigh, tomorrowTempLow, 
-        dayThreeTemp, dayThreeWeatherCondition, dayThreeTempHigh, dayThreeTempLow, 
-        dayFourTemp, dayFourWeatherCondition, dayFourTempHigh, dayFourTempLow)
-
-})
-.catch(err =>{
-    console.error(err)
-})
- 
-
+      threeDayCard(
+        tomorrowTemp,
+        tomorrowWeatherCondition,
+        tomorrowTempHigh,
+        tomorrowTempLow,
+        dayThreeTemp,
+        dayThreeWeatherCondition,
+        dayThreeTempHigh,
+        dayThreeTempLow,
+        dayFourTemp,
+        dayFourWeatherCondition,
+        dayFourTempHigh,
+        dayFourTempLow
+      );
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
 
-function threeDayCard(tomorrowTemp, tomorrowWeatherCondition, tomorrowTempHigh, tomorrowTempLow, 
-                      dayThreeTemp, dayThreeWeatherCondition, dayThreeTempHigh, dayThreeTempLow, 
-                      dayFourTemp, dayFourWeatherCondition, dayFourTempHigh, dayFourTempLow) {
-
-  const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday", "Sunday", "Monday"];
+function threeDayCard(
+  tomorrowTemp,
+  tomorrowWeatherCondition,
+  tomorrowTempHigh,
+  tomorrowTempLow,
+  dayThreeTemp,
+  dayThreeWeatherCondition,
+  dayThreeTempHigh,
+  dayThreeTempLow,
+  dayFourTemp,
+  dayFourWeatherCondition,
+  dayFourTempHigh,
+  dayFourTempLow
+) {
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+    "Monday",
+  ];
   const d = new Date();
-  let tomorrow = weekday[d.getDay()+1];
-  let dayThree = weekday[d.getDay()+2];
-  let dayFour = weekday[d.getDay()+3];
-  console.log(dayFour)
+  let tomorrow = weekday[d.getDay() + 1];
+  let dayThree = weekday[d.getDay() + 2];
+  let dayFour = weekday[d.getDay() + 3];
+  console.log(dayFour);
 
-  const elementsList = document.querySelectorAll("#tomorrowCard, #dayThreeCard, #dayFourCard");
+  const elementsList = document.querySelectorAll(
+    "#tomorrowCard, #dayThreeCard, #dayFourCard"
+  );
   const elementsArray = [...elementsList];
 
-  elementsArray.forEach(element => {
+  elementsArray.forEach((element) => {
     element.classList.add("dayThreeCards");
   });
 
   //tomorrow
 
-    const tomorrowIMG = document.createElement('img');
-    const tomorrowUL = document.createElement('ul');
+  const tomorrowIMG = document.createElement("img");
+  const tomorrowUL = document.createElement("ul");
 
-    document.getElementById("tomorrowCard").appendChild(tomorrowIMG);
-    document.getElementById("tomorrowCard").appendChild(tomorrowUL);
+  document.getElementById("tomorrowCard").appendChild(tomorrowIMG);
+  document.getElementById("tomorrowCard").appendChild(tomorrowUL);
 
-    tomorrowIMG.src = `./images/${tomorrowWeatherCondition}.png`;
+  tomorrowIMG.src = `./images/${tomorrowWeatherCondition}.png`;
 
-    tomorrowArray = [`${tomorrow}`, `${tomorrowWeatherCondition}`, `Forecast Temp: ${tomorrowTemp}&#176;C`, `High: ${tomorrowTempHigh}&#176;C`, `Low: ${tomorrowTempLow}&#176;C`]
+  tomorrowArray = [
+    `${tomorrow}`,
+    `${tomorrowWeatherCondition}`,
+    `Forecast Temp: ${tomorrowTemp}&#176;C`,
+    `High: ${tomorrowTempHigh}&#176;C`,
+    `Low: ${tomorrowTempLow}&#176;C`,
+  ];
 
-    for (var i=0; i<tomorrowArray.length; i++){
-        var li=document.createElement('li');
-        tomorrowUL.appendChild(li);
-        li.innerHTML=li.innerHTML + tomorrowArray[i];
-    }
+  for (var i = 0; i < tomorrowArray.length; i++) {
+    var li = document.createElement("li");
+    tomorrowUL.appendChild(li);
+    li.innerHTML = li.innerHTML + tomorrowArray[i];
+  }
 
-    //dayThree
+  //dayThree
 
-    const dayThreeIMG = document.createElement('img');
-    const dayThreeUL = document.createElement('ul');
+  const dayThreeIMG = document.createElement("img");
+  const dayThreeUL = document.createElement("ul");
 
-    document.getElementById("dayThreeCard").appendChild(dayThreeIMG);
-    document.getElementById("dayThreeCard").appendChild(dayThreeUL);
+  document.getElementById("dayThreeCard").appendChild(dayThreeIMG);
+  document.getElementById("dayThreeCard").appendChild(dayThreeUL);
 
-    dayThreeIMG.src = `./images/${dayThreeWeatherCondition}.png`;
+  dayThreeIMG.src = `./images/${dayThreeWeatherCondition}.png`;
 
-    dayThreeArray = [`${dayThree}`, `${dayThreeWeatherCondition}`, `Forecast Temp: ${dayThreeTemp}&#176;C`, `High: ${dayThreeTempHigh}&#176;C`, `Low: ${dayThreeTempLow}&#176;C`]
+  dayThreeArray = [
+    `${dayThree}`,
+    `${dayThreeWeatherCondition}`,
+    `Forecast Temp: ${dayThreeTemp}&#176;C`,
+    `High: ${dayThreeTempHigh}&#176;C`,
+    `Low: ${dayThreeTempLow}&#176;C`,
+  ];
 
-    for (var i=0; i<dayThreeArray.length; i++){
-        var li=document.createElement('li');
-        dayThreeUL.appendChild(li);
-        li.innerHTML=li.innerHTML + dayThreeArray[i];
-    }
+  for (var i = 0; i < dayThreeArray.length; i++) {
+    var li = document.createElement("li");
+    dayThreeUL.appendChild(li);
+    li.innerHTML = li.innerHTML + dayThreeArray[i];
+  }
 
-    //dayFour
+  //dayFour
 
-    const dayFourIMG = document.createElement('img');
-    const dayFourUL = document.createElement('ul');
+  const dayFourIMG = document.createElement("img");
+  const dayFourUL = document.createElement("ul");
 
-    document.getElementById("dayFourCard").appendChild(dayFourIMG);
-    document.getElementById("dayFourCard").appendChild(dayFourUL);
+  document.getElementById("dayFourCard").appendChild(dayFourIMG);
+  document.getElementById("dayFourCard").appendChild(dayFourUL);
 
-    dayFourIMG.src = `./images/${dayFourWeatherCondition}.png`;
+  dayFourIMG.src = `./images/${dayFourWeatherCondition}.png`;
 
-    dayFourArray = [`${dayFour}`, `${dayFourWeatherCondition}`, `Forecast Temp: ${dayFourTemp}&#176;C`, `High: ${dayFourTempHigh}&#176;C`, `Low: ${dayFourTempLow}&#176;C`]
+  dayFourArray = [
+    `${dayFour}`,
+    `${dayFourWeatherCondition}`,
+    `Forecast Temp: ${dayFourTemp}&#176;C`,
+    `High: ${dayFourTempHigh}&#176;C`,
+    `Low: ${dayFourTempLow}&#176;C`,
+  ];
 
-    for (var i=0; i<dayFourArray.length; i++){
-        var li=document.createElement('li');
-        dayFourUL.appendChild(li);
-        li.innerHTML=li.innerHTML + dayFourArray[i];
-    }
-
+  for (var i = 0; i < dayFourArray.length; i++) {
+    var li = document.createElement("li");
+    dayFourUL.appendChild(li);
+    li.innerHTML = li.innerHTML + dayFourArray[i];
+  }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /* generate a display card on screen which states users location, the weather now, temp, min, max and displays a pciture of corresponding weather conditions*/
-
-
-
-
-
-    
-
